0a6441570429043613768a9e0a0b9eb06607b44a
Felix Chan <felixchan29@gmail.com>
2018-06-17 04:28:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name){
+function happyHolidayTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name){
+function happyHolidayTo(holiday, name){
   return `Happy ${holidays}, ${name}!`
 }
 
-function holidayCountdown(days, holiday){
+function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
 }
